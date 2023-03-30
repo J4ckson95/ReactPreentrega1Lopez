b1d01dbfefed43f493530564960a305f14bae744
Jason López <j4ckson95@hotmail.com>
2023-03-30 09:36:55 -0500

    first commit

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import cart from "./assets/carrito-de-compras.png"
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={cart}></img>
+            0
+        </div>
+    )
+
+}
+export default CartWidget
